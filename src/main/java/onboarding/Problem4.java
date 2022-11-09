96bea2c270287104262ef7c114d64f2a51894d0c
Irang <irang0426@gmail.com>
2022-11-09 15:15:12 +0900

    feat: Problem4 - isConversion2

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Problem4 {
     public static String solution(String word) {
-        return isConversion(word);
+        return isConversion2(word);
     }
 
     public static String isConversion(String str) {
@@ -29,10 +29,10 @@ public class Problem4 {
         int index;
 
         for (int i = 0; i < conArr.length; i++) {
-            if (conArr[i] > 'A' && conArr[i] < 'Z') {
+            if (conArr[i] >= 'A' && conArr[i] <= 'Z') {
                 index = upper.indexOf(String.valueOf(conArr[i]));
                 sb.append(upper_reverse.get(index));
-            } else if (conArr[i] > 'a' && conArr[i] < 'z') {
+            } else if (conArr[i] >= 'a' && conArr[i] <= 'z') {
                 index = lower.indexOf(String.valueOf(conArr[i]));
                 sb.append(lower_reverse.get(index));
             } else {
@@ -40,5 +40,18 @@ public class Problem4 {
             }
         }
         return sb.toString();
+    }
+
+    public static String isConversion2(String str) {
+        char[] ch = str.toCharArray();
+
+        for (int i = 0; i < ch.length; i++) {
+            if (ch[i] >= 'a' && ch[i] <= 'z') {
+                ch[i] = (char)('z' - ch[i] + 'a');
+            } else if (ch[i] >= 'A' && ch[i] <= 'Z') {
+                ch[i] = (char)('Z' - ch[i] + 'A');
+            }
+        }
+        return String.valueOf(ch);
     }
 }
