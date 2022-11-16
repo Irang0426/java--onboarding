69a5915c75f4417fdf0fd247344bb0612808f2c6
Irang <irang0426@gmail.com>
2022-11-16 12:58:08 +0900

    feat: Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,28 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
-        return answer;
+        return getChange(money);
+    }
+
+    public static List<Integer> getChange(int money) {
+        if (!(money >= 1 && money <= 1000000)) {
+            System.out.println("부자시네요.");
+        }
+
+        List<Integer> moneyArr = new ArrayList<>();
+        int[] won = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+        for (int i = 0; i < won.length; i++) {
+            if (money >= won[i]) {
+                moneyArr.add(money / won[i]);
+                money %= won[i];
+            } else {
+                moneyArr.add(0);
+            }
+        }
+        return moneyArr;
     }
 }
